b84244898d281994537378f4ab60e129be14a003
tom-gospooky <74213799+tom-gospooky@users.noreply.github.com>
2023-11-08 09:40:18 -0800

    [ADD][VFX]

--- Assets/VFX_Controller.js
+++ Assets/VFX_Controller.js
@@ -14,16 +14,35 @@
     //@input Component.VFXComponent smoke_VFX
     //@input Component.VFXComponent smoke_burst_VFX
     // @input float smoke_burst_duration = 0 {"label": "Smoke Burst Duration"}
+    //@input bool smake_rim_active = false
+
 //@ui {"widget":"group_end"}
 
+//@ui {"widget":"group_start","label":"Colors"}
+    //@input vec4 color_0 {"widget":"color"}
+    //@input vec4 color_1 {"widget":"color"}
+    //@input vec4 color_2 {"widget":"color"}
+    //@input vec4 color_3 {"widget":"color"}
+    //@input vec4 color_4 {"widget":"color"}
+//@ui {"widget":"group_end"}
 
+var colorCounter = 0;
+var colors = [
+    script.color_0,
+    script.color_1,
+    script.color_2,
+    script.color_3,
+    script.color_4
+];
 
 /** @type {VFXComponent} */
 var smoke_VFX = script.smoke_VFX;
+var smoke_burst_VFX = script.smoke_burst_VFX;
 var confirm_VFX = script.confirm_VFX;
 var fire_VFX = script.fire_VFX;
 
-
+    /** @type {boolean} */
+    var smake_rim_active = script.smake_rim_active;
 
 script.api.setFireStrength = setFireStrength
 script.api.startSmoke = startSmoke
@@ -33,6 +52,7 @@ script.api.onIngredientAdded = onIngredientAdded
 
 let confirm_burst_duration = script.confirm_burst_duration;
 let smoke_burst_duration = script.smoke_burst_duration;
+let smoke_rim_duration = script.smoke_rim_duration;
 
 function setFireStrength(){
 
@@ -40,33 +60,41 @@ function setFireStrength(){
 
 function startSmoke(){
 
+    var burstDur = 1000 + getTime();
+
+    // script.smoke_VFX.asset.properties["rim_active"] = true;
+    script.smoke_VFX.asset.properties["burstDuration"] = burstDur;
+
 }
 
 function endSmoke(){
     
+    var burstDur = 0.1 + getTime();
+    script.smoke_VFX.asset.properties["burstDuration"] = burstDur;
 }
 
 function onLidOpened(){
   
     var burstDur = smoke_burst_duration + getTime();
-
-    print(burstDur)
     script.smoke_burst_VFX.asset.properties["burstDuration"] = burstDur;
 }
 
 /**
  * 
  * @method onIngredientAdded()
- * @param {Object} ingredientData
  * Triggers VFX ring every time an ingredient is succesfully added to cauldron
  */
-function onIngredientAdded(ingredientData){
+function onIngredientAdded(){
     // print("onIngredientAdded")
     var burstDur = smoke_burst_duration + getTime();
 
+    var color = colors[colorCounter%colors.length];
+
     script.confirm_VFX.asset.properties["burstDuration"] = burstDur;
-    script.confirm_VFX.asset.properties["startColor"] = ingredientData.color;
-    script.confirm_VFX.asset.properties["endColor"] = ingredientData.color;
+    script.confirm_VFX.asset.properties["startColor"] = color;
+    script.confirm_VFX.asset.properties["endColor"] = color;
+
+    colorCounter++;
 }
 
 
@@ -86,3 +114,8 @@ function simulateOpeningLid(){
 
     onLidOpened()
 }
+/* 
+script.createEvent("TouchStartEvent").bind(function(eventData){
+    print("hallo")
+    onIngredientAdded();
+}); */
